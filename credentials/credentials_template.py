32725f6e8411c89ef65ebbff9144de960784dd34
goranaviani <goranaviani@gmail.com>
2019-08-16 21:18:28 +0200

    credentials template

---
+++ credentials/credentials_template.py
@@ -0,0 +1,6 @@
+#Remove _template from the name and fill variables with proper data
+
+my_account_sid = "your account sid"
+my_auth_token = "your auth token"
+my_telephone = "your receiving number"
+my_service_telephone = "your outgoing number"
